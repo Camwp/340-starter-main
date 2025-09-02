98168c80d6bfbdc740d02dc6432dc6ab0236b5dc
CWP <77127867+Camwp@users.noreply.github.com>
2025-09-02 12:03:00 -0700

    layout & partials

--- server.js
+++ server.js
@@ -9,12 +9,18 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -26,5 +32,5 @@ const host = process.env.HOST
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on http://${host}:${port}`)
 })

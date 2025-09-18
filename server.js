a00f90e1e139fbd6a4b95060c36e8e85decba3fc
CWP <77127867+Camwp@users.noreply.github.com>
2025-09-17 19:44:37 -0700

    changes to routes

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute.js")
+const utilities = require("./utilities/index.js")
+
 const express = require("express")
 const env = require("dotenv").config()
 const path = require("path");
@@ -29,11 +33,31 @@ app.use(static)
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // serve /public (so /images/... and /css/... work)
 app.use(express.static(path.join(__dirname, "public")))
+
+
+app.use("/inv", inventoryRoute)
+
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status == 404) { message = err.message } else { message = 'Oh no! There was a crash. Maybe try a different route?' }
+  res.render("errors/errors", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
